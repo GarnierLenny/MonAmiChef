4cd38ed72be8f921bf1040f9b763e857347ca2e9
Lenny Garnier <lenny.garnier00@gmail.com>
2025-09-20 14:22:56 +0200

    responsive

--- frontend/src/components/meal-plan/NewMobileMealLayout.tsx
+++ frontend/src/components/meal-plan/NewMobileMealLayout.tsx
@@ -65,7 +65,7 @@ export const NewMobileMealLayout = ({
   };
 
   return (
-    <div className="flex flex-col h-full w-full bg-orange-500 pb-15 overflow-hidden">
+    <div className="flex flex-col h-full w-screen pb-15 bg-transparent overflow-hidden">
       {/* Progress Card */}
       <div className="p-4">
         <ProgressCard
